2ac31bfcb7f16c4f30dc75e9dd8158d88bc43d6c
jordi <jordi@192.168.1.137>
2016-04-16 13:20:41 +0200

    Apanyant les linees per l'apartat de RESOLUCIO DE CONFLICTES

--- Prjava02/src/prjava02/Prjava02.java
+++ Prjava02/src/prjava02/Prjava02.java
@@ -1,12 +1,19 @@
 package prjava02;
+
 import java.io.*;
 import java.net.*;
+
 public class Prjava02 {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("versió 0.1 del projecte prjava02");
+		
 		try {
 			InetAddress adreça = InetAddress.getLocalHost();
+			
 			String hostname = adreça.getHostName();
+			
 			System.out.println("hostname=" + hostname);
 			System.out.println("Nom de l'usuari: "
 					+ System.getProperty("user.name"));
@@ -21,6 +28,7 @@ public class Prjava02 {
 			System.out.println("Afegint més codi a la "
 					+ "branca00 del projecte prjava02");
 		} catch (IOException e) {
+			
 		}
-	}
+	}	
 }
